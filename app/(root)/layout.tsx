fc93476ff8be71b878c83e1f86b281ba3038fddf
leeonM <110701138+leeonM@users.noreply.github.com>
2025-03-31 05:10:12 +0100

    implement qstash

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,24 +1,45 @@
-import { auth } from '@/auth'
-import Header from '@/components/Header'
-import { redirect } from 'next/navigation';
-import React, { ReactNode } from 'react'
+import { auth } from "@/auth";
+import Header from "@/components/Header";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { redirect } from "next/navigation";
+import { after } from "next/server";
+import React, { ReactNode } from "react";
+import { eq } from "drizzle-orm";
 
-const Layout = async ({children}:{children:ReactNode}) => {
-
+const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
 
-  if (!session) redirect('/sign-in');
+  if (!session) redirect("/sign-in");
+
+  // update last activity when user logs in
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    // get user and see if last activity is today
+    const user = await db
+    .select()
+    .from(users)
+    .where(eq(users.id, session?.user?.id))
+    .limit(1)
+
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0,10)) return;
+
+    // get date without time
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session?.user?.id));
+  });
 
   return (
-    <main className='root-container'>
-      <div className='mx-auto max-w-7xl'>
+    <main className="root-container">
+      <div className="mx-auto max-w-7xl">
         <Header session={session} />
-         <div className='mt-20 pb-20'>
-            {children}
-         </div>
+        <div className="mt-20 pb-20">{children}</div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
