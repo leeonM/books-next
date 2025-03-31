c4d2764cf958ea79f8685c0202ac48a2bf36970f
leeonM <110701138+leeonM@users.noreply.github.com>
2025-03-31 04:38:33 +0100

    first commit

---
+++ types.d.ts
@@ -0,0 +1,41 @@
+export interface Book {
+    id: string;
+    title: string;
+    author: string;
+    genre: string;
+    rating: number;
+    totalCopies: number;
+    availableCopies: number;
+    description: string;
+    coverColor: string;
+    coverUrl: string;
+    video: string;
+    summary: string;
+    isLoanedBook?: boolean;
+  }
+  
+  export interface AuthCredentials {
+    fullName: string;
+    email: string;
+    password: string;
+    universityId: number;
+    universityCard: string;
+  }
+  
+  export interface BookParams {
+    title: string;
+    author: string;
+    genre: string;
+    rating: number;
+    coverUrl: string;
+    coverColor: string;
+    description: string;
+    totalCopies: number;
+    videoUrl: string;
+    summary: string;
+  }
+  
+  export interface BorrowBookParams {
+    bookId: string;
+    userId: string;
+  }
